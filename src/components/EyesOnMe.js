7934639d200b928e5b8e8e40441c00aa5f40e0b2
Derek Townsend <derekjavon2@gmail.com>
2019-07-17 16:10:48 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+// import Filters from './Filters'
+// import PetBrowser from './PetBrowser'
+
+export default class EyesOnMe extends React.Component {
+  focusOnMe = () => {
+    console.log("Good!");
+  }
+
+  blurOnMe = () => {
+    console.log("Hey! Eyes on me!");
+
+  }
+  render(){
+    return (
+      <div>
+      <button onFocus={this.focusOnMe} onBlur={this.blurOnMe}></button>
+      </div>
+    )
+  }
+}
